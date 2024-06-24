fdd175310d8c18a56940e54839936fe0df480def
MarcoSkiadas <Skiadas2@hotmail.com>
2024-06-24 10:46:29 +0200

    Adding ControllerTests

--- src/test/java/org/example/springrecap12406/Controller/RecapControllerTest.java
+++ src/test/java/org/example/springrecap12406/Controller/RecapControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -49,7 +50,55 @@ class RecapControllerTest {
 """));
     }
     @Test
-    void getToDos_shouldReturnAllToDos_whenCalled() throws Exception {}
+    void addToDo_shouldReturnToDo_whenAdded() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""" 
+{        "description": "To Do 2",
+        "status": "OPEN" }
+"""))
+                .andExpect(MockMvcResultMatchers.status().isOk());
 
+    }
+    @Test
+    void deleteToDo_shouldDeleteToDo_whenDeleted() throws Exception {
+repo.save(new ToDo("1", "To Do 2", Status.OPEN));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/todo/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""" 
+{        "description": "To Do 2",
+        "status": "OPEN" }
+"""))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+    @Test
+    void updateToDo_shouldUpdateToDo_whenUpdated() throws Exception {
+        repo.save(new ToDo("1", "To Do 2", Status.OPEN));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/todo/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""" 
+{        "id": "1",
+"description": "To Do 2",
+        "status": "IN_PROGRESS" }
+"""))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+    @Test
+    void getToDetail_shouldReturnToDo_whenCalledToDo() throws Exception {
+        //GIVEN
+        repo.save(new ToDo("1", "To Do 2", Status.OPEN));
+        //WHEN & THEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+    {
+        "id": "1",
+        "description": "To Do 2",
+        "status": "OPEN"
+    }
+"""));
+    }
 
 }
